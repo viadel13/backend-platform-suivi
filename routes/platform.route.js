6fe3773e0e05bce56ccdfb0837cc4e744e2f50d7
viadel13 <shababizetre@gmail.com>
2023-12-14 17:00:18 +0100

    first commit

--- routes/platform.route.js
+++ routes/platform.route.js
@@ -3,6 +3,5 @@ const platformController = require('../controller/platform.controller');
 const router = express.Router();
 
 router.post('/register', platformController.register);
-router.post('/signIn', platformController.signIn);
 
 module.exports = router;
